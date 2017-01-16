bb06f8aee5d0d4ff90f12d4e0ef67a8551b38054
Brian Johnston <b_johnston98@yahoo.com>
2017-01-16 16:53:45 -0500

    two, no errors

--- MinedMindsfunction.rb
+++ MinedMindsfunction.rb
@@ -1,4 +1,9 @@
 def get_mined_minds_result(number)
-	1
-	#placing 1 makes func work, but no scrnecho
+	number
+	#placing 1 makes func work, no scrnecho
+end
+
+def get_mined_minds_result(number)
+	number
+	#placing 1 makes func work, no scrnecho
 end
