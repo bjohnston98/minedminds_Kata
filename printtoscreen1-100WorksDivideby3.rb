f9015ff30bfdd20ea5b4526012874ab914f37c1c
Brian Johnston <b_johnston98@yahoo.com>
2017-01-16 14:03:59 -0500

    refactor, cleanup

--- printtoscreen1-100WorksDivideby3.rb
+++ printtoscreen1-100WorksDivideby3.rb
@@ -1,11 +1,10 @@
 number = 1
 
 	100.times do
-	
-	if number == 3
-		puts "mined"
-	else
-		puts number
+		if number == 3
+			puts "mined"
+		else
+			puts number
+		end
 		number = number + 1
 	end
-end
